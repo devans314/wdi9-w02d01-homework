65ca500c2dfce95758aa932c580b3240b13503d4
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-09-17 17:33:29 -0600

    Data Types

---
+++ js/app.js
@@ -0,0 +1,78 @@
+// 1. Data types
+// Refresher
+// So far we have seen:
+
+// Strings
+// Numbers
+// Booleans
+// Arrays
+// Objects
+// When these datatypes are combined, we get a data structure, 
+// for example, an array that contains objects that each contain booleans.
+
+// Suppose you are tasked with creating some software, 
+// and it is up to you to determine which datatypes 
+// and what data structure to use. For each of the following, 
+// write which data types you would use to represent the data, 
+// and then give a small example of the data structure:
+
+// A light switch that can be either on or off.
+// A user's email address.
+// A spaceship with a hull, laser blasters, tractor beam, 
+// and warp drive.
+// A list of student names from our class.
+// A list of student names from our class, each with a location.
+// A list of student names from our class, 
+// each with a location and each with a list of favorite tv shows.
+
+// -boolean-
+// if(x === true){
+//   console.log('The lights are on!);
+//} else {
+//     console.log('false');
+//}
+// - string data type-
+// const userEmail = ['bertha.pg@gmail.com', 'jimmy.john@aol.com']
+// 
+//-object with properties-
+// const spaceShip = {
+//     hull: "titanium",
+//     weapon: "laser blasters",
+//     tractorBeam: true,
+//     warpDrive: true
+// }
+
+//-array of strings-
+// const students = ['Donnie', 'Deirdre', 'Liz', 'Tomas', 'Deeter'
+// ];
+
+// const students = {
+//     Donnie: 'Denver', 
+//     Deirdre: 'Denver',
+//     Liz: 'Denver', 
+//     Tomas: 'Denver', 
+//     Deeter: 'Denver'
+// ];
+
+// const students = {
+//     const donnie = {
+//         location: 'Denver', 
+//         favShow: 'The Wire'},
+//     const deirdre = {
+//         location: 'Denver',
+//         favShow: 'The Bachelor'
+//     },
+//     const liz = {
+//         location: 'Denver', 
+//         favShow: 'Gameofthrones'},
+//     const Tomas = {
+//         location: 'Denver', 
+//         favShow: 'BreakingBad'
+//     }
+// }
+
+
+
+
+
+
