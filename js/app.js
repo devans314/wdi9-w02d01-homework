7c9d025693c09940bcc6d837ccb6fed55196f8c1
Donald Evans <donaldevans@Donalds-MacBook-Pro.local>
2018-09-17 18:14:02 -0600

    oddBonds

--- js/app.js
+++ js/app.js
@@ -198,10 +198,18 @@ const bondFilms = [
   { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
 ];
 
-const bondTitles = []
-for(i = 0; i < bondFilms.length; i++){
-    bondTitles.push(bondFilms[i].title)
-};
-console.log(bondTitles);
+// const bondTitles = []
+// for(i = 0; i < bondFilms.length; i++){
+//     bondTitles.push(bondFilms[i].title)
+// };
+// console.log(bondTitles);
 
+const oddBonds = [];
+
+for(i = 0; i < bondFilms.length; i++){
+    if(bondFilms.year % 2 !== 0){
+        oddBonds.push(bondFilms[i].year)
+    }
+}
+console.log(oddBonds);
 
